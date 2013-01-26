c65f6db7ddecd5cc5210e357caa333e79327b141
Aymeric Barthe <aymeric@barthe.ph>
2013-01-26 22:15:46 +0000

    Added ajax/config.php page to retrieve configuration dynamically from javascript.

--- php/core/config.php
+++ php/core/config.php
@@ -3,6 +3,9 @@ namespace Barthe\Exposition;
 
 class Config 
 {
+	const INFO = 'Exposition. (c) 2013 Aymeric Barthe';
+	const VERSION = 'alpha';
+
 	const PHOTO_DIR = '/nas/media3/Exposition/Albums';
 	const CACHE_DIR = '/nas/media3/Exposition/Cache';
 	static public function PHOTO_EXTENSIONS() 
