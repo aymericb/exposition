7f099aa04e9569dba18b8c7c746bac7d73474f7d
Aymeric Barthe <aymeric@barthe.ph>
2013-04-02 22:44:49 +0100

    Added automated processing of HTML files with Handlebars to generate appropriate SCRIPT elements.

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,23 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        html: {
+            options: {
+                templateData: {
+                    scripts: [
+                        'js/<%= pkg.name %>.min.js'
+                    ]
+                }
+            },
+            index: {
+                src: 'index.html',
+                dest: 'build/index.html'
+            },
+            update: {
+                src: 'update.html',
+                dest: 'build/update.html'
+            }
+        },
         concat: {
             options: {
                 // define a string to put between each file in the concatenated output
@@ -26,11 +43,24 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.registerMultiTask('html', 'Generate HTML files based on Handlebar templates.', function() {
+        var Handlebars = require('handlebars');
+        var options = this.options();
+        var templateData = options.templateData || {};
+        //grunt.log.writeln(this.target + ': ' + JSON.stringify(this.data));
+        this.files.forEach(function(f) {
+            grunt.log.writeln('Source file "' + f.src + '"');
+            grunt.log.writeln('Dest file "' + f.dest + '"');
+            var template = Handlebars.compile(grunt.file.read(f.src));
+            grunt.file.write(f.dest, template(templateData));
+        });
+    });
+
     // Load plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['html', 'concat', 'uglify']);
 
 };
