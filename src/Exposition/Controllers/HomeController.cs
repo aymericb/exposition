6d3cc3090f4e89cdbcbe143bf4482778b13ad961
Aymeric Barthe <aymeric@barthe.ph>
2016-11-20 00:26:10 +0000

    Added support for appsettings.json configuration.

--- src/Exposition/Controllers/HomeController.cs
+++ src/Exposition/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics.Contracts;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,18 @@ namespace Exposition
 {
     public class HomeController : Controller
     {
+        private readonly IOptions<Models.Config> config;
+
+        public HomeController(IOptions<Models.Config> config)
+        {
+            Contract.Requires(config != null);
+            this.config = config;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(this.config.Value);
         }
     }
 }   
