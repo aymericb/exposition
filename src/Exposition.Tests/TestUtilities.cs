02490b547428c83ec1a73e451114dc295fc7c6ac
Aymeric Barthe <aymeric@barthe.ph>
2016-11-26 22:39:42 +0000

    Added empty test project.

---
+++ src/Exposition.Tests/TestUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exposition.Tests
+{
+    [TestClass]
+    public class TestUtilities
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
