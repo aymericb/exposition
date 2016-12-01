022b46b2f88c3e98ca5df5f58724630cd2371d94
Aymeric Barthe <aymeric@barthe.ph>
2016-12-01 18:10:53 +0000

    Added FileProvider service.

---
+++ src/Exposition/Services/IFileProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Exposition.Services.FileProvider;
+
+namespace Exposition.Services
+{
+    public interface IFileProvider
+    {
+        ItemType GetItemType(string path);
+        IEnumerable<string> GetAlbumChildren(string path);
+    }
+}
